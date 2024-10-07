ca4dffe0f483c65779e70d21ac23c459a8ec3ca0
Costanza Pasquotto Assef <81304267+Costanza22@users.noreply.github.com>
2024-10-06 22:04:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,190 @@
+const livros = {
+            'front-end': [
+                {
+                    titulo: "Padrões Javascript",
+                    imagem: "https://th.bing.com/th/id/OIP.D8tuC6CQP1z1kKjyM7PfPwAAAA?w=322&h=445&rs=1&pid=ImgDetMain",
+                    editora: "Novatec",
+                    ano: 2014,
+                    ondeVender: "https://www.amazon.com.br/Padr%C3%B5es-Javascript-Stoyan-Stefanov/dp/857522266X/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=FMIW9KELTIE1&dib=eyJ2IjoiMSJ9.D_fkQfCeR_78tvqm05qYf6225m2hK_15kajl1PXruUvnqMJcvpvmS6UjM58sjp3QhCnLNomErOKgKDw7oaJljlxfFPKBoJIRkiix33_Mc4YBzUlW6X7ZdARH9SEa_SxQo0A7Y1IZkSOdPyLYUQcCwUEOxbIICegZn98HSh5dkMGArUzJg1f9gg59GeiDN8g4m-B0_0vhbjHvI4GwGZkJ8VUALXlpTxIQ0GdgkqTz2GM.4rcYWp9YQaKU3s0RE5zIbZVSNmhIg7RxVQrkO8onOZQ&dib_tag=se&keywords=Padr%C3%B5es+Javascript&qid=1728261547&s=books&sprefix=padr%C3%B5es+javascript%2Cstripbooks%2C225&sr=1-1"
+                },
+                {
+                    titulo: "Criando Aplicações Web com Wordpress",
+                    imagem: "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575223772.jpg",
+                    editora: "Novatec",
+                    ano: 2015,
+                    ondeVender: "https://www.amazon.com.br/Criando-Aplica%C3%A7%C3%B5es-Web-com-Wordpress/dp/8575223771/ref=sr_1_2?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=25QKOKK32EMXC&dib=eyJ2IjoiMSJ9.kV6ic15y8DPGCmStW7ALxAgSrniipmfWc6xhbhUmK_B_2ExkQ1XHnU45a8JE4ttHj9f6wgELrTeBJ8POh3YjKQuRApVfoRP--1U17jRuurBseawmR2-a8PRq-0IJsDt4k0fhMLLPKj6WEt7VEVohzcwloaa5ADGW-8agPgYRPGsEb0sU6rAKwnrB3exaKNtcDEqX1ms_SeYLE5eiATs3xHYSTDHOwQBxbg7SM90mkAU.HCV020oIbGWWzBU9mM5ZIRDmRKXX0_WxmYP1kEcWBos&dib_tag=se&keywords=Criando+Aplica%C3%A7%C3%B5es+Web+com+Javascript&qid=1728261573&s=books&sprefix=padr%C3%B5es+javascript%2Cstripbooks%2C201&sr=1-2"
+                },
+                {
+                    titulo: "Angular Up and Running",
+                    imagem: "https://m.media-amazon.com/images/I/51XAcKnSJKL._SL500_.jpg",
+                    editora: "O'Reilly Media",
+                    ano: 2016,
+                    ondeVender: "https://www.amazon.com.br/Angular-Running-Learning-Step-English-ebook/dp/B07DFTFMRK/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=36WEEUHMQVJR7&dib=eyJ2IjoiMSJ9.C_GwpKaecu-Drz_ZEQlpnjmeo7inq1sRKoiIh7ko7clptI3cv9edgwsP5twbqV79UXK6e2uHfU6jDr5z_DlbzG9PfT_2VVlf7RwJ6ttmMfuw20Sz-wWeh9W5A8kbygPByLywVFv004COGmzd8mJ_WdhXefY9-PgjAxHsD7JTzO6q_31HVjRAnoW2vqRAMcFtfCVvAxbZ1mH6CYMHdu-5r8FBWUQWPdj-vKWK3Yeh_Yw.Z4scOj6QKIYSM2M_-uVMhgcsAq7CPqRTC3fvoPlgDq4&dib_tag=se&keywords=Angular+Up+and+Running&qid=1728261623&s=books&sprefix=angular+up+and+running%2Cstripbooks%2C260&sr=1-1"
+                },
+                {
+                    titulo: "Pense em Python",
+                    imagem: "https://m.media-amazon.com/images/I/51yjRvjhSIL._SX357_BO1,204,203,200_.jpg",
+                    editora: "Novatec",
+                    ano: 2013,
+                    ondeVender: "https://www.amazon.com.br/Pense-Python-Como-Cientista-Computa%C3%A7%C3%A3o/dp/8575225081/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=190CYHL22A823&dib=eyJ2IjoiMSJ9.Sz2yeWpC1Pka-UIun85_Oc6RI6pcR4owlsyAWo-Ej0qII7DrjApBJgceHZzDlF9i58wS21fQWU8krsHPwy9TzTZFAQTLZstgCWup9-mQdfYaqEpNTtJUgpB84F0PdawtujlFzRzdn2LHYXVW9Y63l9XpkAnoekumX5DS4Qp8gHumQsXqlOE4BcBi0oVHehST6PcgYZkn7Rzpy5fLAHyCWkp4qZVq1q8BdL8yEr1lG5c.F3QiAgXMVaW5VeXqTvLsJkcMVIfR7aBJ0cDnqWESZiY&dib_tag=se&keywords=Pense+em+Python&qid=1728261642&s=books&sprefix=angular+up+and+running%2Cstripbooks%2C200&sr=1-1"
+                }
+            ],
+            'back-end': [
+                {
+                    titulo: "Aprendendo Node",
+                    imagem: "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575225400.jpg",
+                    editora: "Novatec",
+                    ano: 2017,
+                    ondeVender: "https://www.amazon.com.br/Aprendendo-Node-Usando-JavaScript-Servidor/dp/8575225405/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=34DKTCGXQRHFD&dib=eyJ2IjoiMSJ9.9Va-9Q-5VWCHwDw_gclNWNNPxmZYn2fARWsG2yeNiiCvfbvanxxGNyF8kZVM7rGob87k-plwN2uXyu7zO2D5jYzbD38YxGYosS5YQ2ZenpubfJea-M5v7eI1sVDrtsAdObR0OK3FbTHllQvtK-_Or2y9ryhYveACaZF3WP1TtZliCzvvVZhOIWY3qtjhD96You4LpYjPtHNwhCXM0p2sVNSQ3AtNxR0QyG_0XrwsxM4.0EiMd9Cg_dJDWP_5yTY4DP9K-TmJCIWMc3DVWmAlTF4&dib_tag=se&keywords=Aprendendo+Node&qid=1728261661&s=books&sprefix=aprendendo+node%2Cstripbooks%2C218&sr=1-1"
+                },
+                {
+                    titulo: "Desenvolvendo com Laravel",
+                    imagem: "https://2.bp.blogspot.com/-bZAMwmaoT10/WRrheg2uq0I/AAAAAAAAGbo/-y_8-skdJugdS3B3YW_L2rOg_gias-SvwCLcB/s1600/Capa%2B-%2BDesenvolvendo%2Bcom%2BLaravel.jpg",
+                    editora: "Novatec",
+                    ano: 2016,
+                    ondeVender: "https://www.amazon.com.br/Desenvolvendo-Laravel-Framework-Constru%C3%A7%C3%A3o-Aplicativos/dp/8575225677/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2P5D3Y7LVWNQC&dib=eyJ2IjoiMSJ9.lefQ5rKoBZ2rljxKky8Qzo0Qzh38QXkv3xNxrQQZSo8UhZotcEMQ39rUycXNiJ3L.x48cwabOrjFWDvT2hJjY7lD3gIgoWlNnUHKzPs6-7_U&dib_tag=se&keywords=Desenvolvendo+com+Laravel&qid=1728261680&s=books&sprefix=aprendendo+node%2Cstripbooks%2C260&sr=1-1"
+                },
+                {
+                    titulo: "PHP Moderno",
+                    imagem: "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224281.jpg",
+                    editora: "Novatec",
+                    ano: 2021,
+                    ondeVender: "https://www.amazon.com.br/Modern-PHP-Josh-Lockhart/dp/1491905018/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3VG0IIOQOJW2W&dib=eyJ2IjoiMSJ9.vmNly8zLr5FH-H8fajJFPVRUaUQsruhOr759208A3w24WpHFBpmWBNxJwlC0IJQOXOJ0oVg-ZngXL6OmCC4xmKOieX8A-qXha5fdRay36Tnr0DOpoZHlWCtakZzCNybyW0MZx6VjDjpLDAChaEpJlhMWFAY2QnSYVS_6Fr0Dw9a_U-aH_no476wEXPBapcVhayPsSIeJAWMKqp77JqakHkCDpYnMR75ILkD3Brpcbh8.z09OgGXtjR2TQd7lLS2v0knOgYZ4H28THiFzYcQYhQc&dib_tag=se&keywords=PHP+Moderno&qid=1728261703&s=books&sprefix=desenvolvendo+com+laravel%2Cstripbooks%2C209&sr=1-1&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d"
+                }
+            ],
+            biografia: [
+                {
+                    titulo: "A Cabeça de Steve Jobs",
+                    imagem: "https://d2r9epyceweg5n.cloudfront.net/stores/174/533/products/31-livrojobs-11-0e1a86292fe63b84b615131760932455-1024-1024.jpg",
+                    editora: "Leya",
+                    ano: 2011,
+                    ondeVender: "https://www.amazon.com.br/Cabe%C3%A7a-Steve-Jobs-Leander-Kahney/dp/8522013225"
+                },
+                {
+                    titulo: "Os Heróis da Revolução",
+                    imagem: "https://images.cdn2.buscalibre.com/fit-in/360x360/55/3c/553c0f7d40cd6a9684120f2bd8e9699e.jpg",
+                    editora: "Valentina",
+                    ano: 2017,
+                    ondeVender: "https://www.amazon.com.br/Os-her%C3%B3is-revolu%C3%A7%C3%A3o-Wozniak-Zuckerberg/dp/8563993321/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=KE3QR98JVA26&dib=eyJ2IjoiMSJ9.jjneZ4DJvSTJmLRSZUQJ5E7bBnTUBcmwOX4RDBFJSewwyqe8GKQtQhMM8k_rc9ILCY0RNoAKwxTz7Nf-NzwiiJPKN-dQT6EF4JYq3D1XiEMkE9GROmCwDJH4a2f7jKWFnOM_SRrSUjbAxkkNsz81gp0H6ycKPFx_wZ-_na5-lhFXRNNCONTateiKz_OKTYqs6bucTTq-HhvioXDEigCuuLo2FGpyiNz9TV7wGGFGC8M.l5Kb5i7r7YFZja2oHL5S6Axupgxu8AQWvvk83HCX770&dib_tag=se&keywords=os+her%C3%B3is+da+revolu%C3%A7%C3%A3o&qid=1728261476&s=books&sprefix=os+herois+da+revolu%C3%A7%C3%A3%2Cstripbooks%2C216&sr=1-1&ufe=app_do%3Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9"
+                },
+                {
+                    titulo: "Tim Cook",
+                    imagem: "https://th.bing.com/th/id/OIP.0hff8dQeqqXJqaW-IqDa9gAAAA?w=310&h=445&rs=1&pid=ImgDetMain",
+                    editora: "Companhia das Letras",
+                    ano: 2015,
+                    ondeVender: "https://www.amazon.com.br/Tim-Cook-G%C3%AAnio-Mudou-Futuro-ebook/dp/B07YL7YTCM/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=6HZ1W4OFB3YA&dib=eyJ2IjoiMSJ9.fJmAP7xWBzOE9W8bWfDWi41mlorh7LKmNBFHV4lLiVx2pYvvwmLC8fm9P2zM0PX_ByHzDUaRilqnbfRXLJqNXXqGBCGMRzzl7TdoVF8s9yisMTnSCOoxPZjG75O41EjMGsVbhcCXci3tqLebkBaF1EUnhCgKFGM1jVNYVIT8rTSxDhLkUsg1yno17H-29QPp333GqaGFzjdqMnpPcrlV3QqYaspAHum4oQdoJXVdrnA.CS_91k_8Xyrxq8bfzugRcVZq9Sk_2H4myLABKpM6eb0&dib_tag=se&keywords=tim+cook&qid=1728261512&s=books&sprefix=tim+cook%2Cstripbooks%2C207&sr=1-1"
+                },
+                {
+                    titulo: "The Google Guys",
+                    imagem: "https://th.bing.com/th/id/OIP.nLVeUhIA-MdMkd8evfrt5wHaLW?rs=1&pid=ImgDetMain",
+                    editora: "Apress",
+                    ano: 2008,
+                    ondeVender: "https://www.amazon.com.br/Google-Guys-Inside-Brilliant-Founders/dp/1591844126/ref=sr_1_2?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=20WQMK7X63MRE&dib=eyJ2IjoiMSJ9.lLuD-jdnF8c3qTxdzFIFitF2_Qt9aIznif1Gipz5fu1I6hAW5p7f9p_k0jlIQm2tJAswFtVIKKIplOTd_6K7ndBaidnGqL29JwHQzX_buF77w12pWbBd-3ySa1eJQaZUrlGhNq43Ie-ZVDi9eUvot161cYc5QnOgn9CLnGSt4G-BYZkQl8axUwzYjcYRfKMAkxCF1wSPhG1Yt2OcN0bdVpQgjyOhBklr3dl5I4AwDTc.LZifMk8mb57fQ_SHk3t90LxyLCGFsZEEPUr01TbhEkM&dib_tag=se&keywords=The+Google+Guys&qid=1728261399&s=books&sprefix=the+google+guys%2Cstripbooks%2C228&sr=1-2&ufe=app_do%3Aamzn1.fos.a492fd4a-f54d-4e8d-8c31-35e0a04ce61e"
+                }
+            ],
+            seguranca: [
+                {
+                    titulo: "Segurança para desenvolvedores web",
+                    imagem: "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224847.jpg",
+                    editora: "Novatec",
+                    ano: 2022,
+                    ondeVender: "https://www.americanas.com.br/produto/126138371/livro-seguranca-para-desenvolvedores-web-usando-jafascript-html-e-css"
+                }
+            ],
+            'conhecimento-geral': [
+                {
+                    titulo: "Código Limpo ",
+                    imagem: "https://imgv2-2-f.scribdassets.com/img/document/678243907/original/1ef10ed174/1698472770?v=1",
+                    editora: "Alta Books",
+                    ano: 2009,
+                    ondeVender: "https://www.amazon.com.br/C%C3%B3digo-limpo-Robert-C-Martin/dp/8576082675#:~:text=%22C%C3%B3digo%20Limpo%22%20%C3%A9%20um%20livro%20indispens%C3%A1vel%20para%20desenvolvedores,guia%20para%20elevar%20o%20n%C3%ADvel%20de%20qualquer%20desenvolvedor."
+                },
+                {
+                    titulo: "Entendendo Algoritmos: Um Guia Ilustrado Para Programadores e Outros Curiosos ",
+                    imagem: "https://m.media-amazon.com/images/I/71Vkg7GfPFL._SL1296_.jpg",
+                    editora: "Novatec",
+                    ano: 2007,
+                    ondeVender: "https://www.amazon.com.br/Entendendo-Algoritmos-Ilustrado-Programadores-Curiosos/dp/8575225634/ref=pd_bxgy_d_sccl_2/143-9562874-3847634?pd_rd_w=xQpk7&content-id=amzn1.sym.24c12540-16c9-4f25-a9fc-1d03ac7f6922&pf_rd_p=24c12540-16c9-4f25-a9fc-1d03ac7f6922&pf_rd_r=MM08MT6KY1SSFG9JTRKY&pd_rd_wg=EpxYf&pd_rd_r=7df6f564-36b7-4691-af30-e2ed25e2e0f5&pd_rd_i=8575225634&psc=1"
+                },
+                {
+                    titulo: "Vida Após o Google: a Queda do big Data e a Ascensão da Economia Blockchain",
+                    imagem: "https://m.media-amazon.com/images/I/61M3rLPo1OS._SL1500_.jpg",
+                    editora: "Alta Cult",
+                    ano: 2021,
+                    ondeVender: "https://www.amazon.com.br/Vida-Ap%C3%B3s-Google-Ascens%C3%A3o-Blockchain/dp/8550813966/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=39RLZWADBW7NK&dib=eyJ2IjoiMSJ9.rMmcmKD7Q_oj4Eca_RbSOV8ccwR02tJbEwWgsN10_JmYpO4vRVBvGvj6DsvWPcP5TYAdXTfCIBs6tjX_lHXC0HzzU1tiHywES0SFoy32nyaUVowjxnMac40_vEpxYYama-vu6d6V786jw5sY-Tr-mrpCeA3qc4VLMfoF1-44wyqaUL0rrk_7l_UpUiySjFiTEh5Qus4iS0LI52dHsyy7sQ.DEFid2tdw5BmZmtWLVaTLUfivxEv4vK5ziZmjS7WPZo&dib_tag=se&keywords=a+vida+ap%C3%B3s+o+google&qid=1728260357&s=books&sprefix=a+vida+apos+o+googl%2Cstripbooks%2C208&sr=1-1"
+                },
+                {
+                    titulo: "Os Quatro: Apple, Amazon, Facebook e Google. O Segredo dos Gigantes da Tecnologia",
+                    imagem: "https://m.media-amazon.com/images/I/71nTb+nJzLL._SL1500_.jpg",
+                    editora: "Alta Books",
+                    ano: 2021,
+                    ondeVender: "https://www.amazon.com.br/Os-Quatro-Facebook-Gigantes-Tecnologia/dp/855081458X/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2U1IN1G44L8C5&dib=eyJ2IjoiMSJ9.ju2T9p6V1xyZpBAcmqwh_3Cg2TrWJqQvvQzLqdozFznqcSMk55A9yN4nYYApc3Lz-Wwl90btDa1YyZVoJ0zVTj_oF-k9Z09XCE131xrPUtriF8ki4PKE4ldNEkpOH9e-okIIuE7yd6Rt7tnTH8u0CvfX3XUHC-4uLmCq0evakqNpfv3TIo-tANucrNPiLFTS9vrukS4_NA3QE4EVwh9-Qre4PszEq8he986q1saNCPA.430-g1v_rFmTiwAUvFqrsJKw9IkEJSva56QPTkkwOx4&dib_tag=se&keywords=os+quatro&qid=1728260470&s=books&sprefix=os+quatr%2Cstripbooks%2C231&sr=1-1"
+                },
+                {
+                    titulo: "Os segredos do Vale do Silício",
+                    imagem: "https://m.media-amazon.com/images/I/91Q-gPDlO8L._SL1500_.jpg",
+                    editora: "Alta Books",
+                    ano: 2021,
+                    ondeVender: "https://www.amazon.com.br/Os-segredos-Vale-Sil%C3%ADcio-aprender-ebook/dp/B085PYX6PR/ref=sr_1_5?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3P389C3076K7&dib=eyJ2IjoiMSJ9.BeKB--v4q3KVSDhYgWdbTjICd_LnmlT3S8Aki0HN1Btp7b2Fodf_seubERZY29cp6kGQ1NidLXej8x31LxZHcsRfUyoMSHOxWli-aHe7zxXNB1ko9DYZuBAsozGalFJ-Yyz5-uNfU_mMjX0EHhHrvXUTxkAN1yVuklpvUo1E2Qwi2fgBVDeHQuJPkx3yOuUbsBDQ8EQ6LdEdWejoG2BBmjgI4RSL9SarmAacEIihSws.SNi0ZVtISOLYQd2SEjKe5RJVMkD3bP2HV2EnYp0ipoI&dib_tag=se&keywords=o+vale+do+silicio&qid=1728260595&s=books&sprefix=o+vale+do+silici%2Cstripbooks%2C256&sr=1-5"
+                }
+
+
+
+            ],
+            'banco-de-dados': [
+                {
+                    titulo: "Introdução à Linguagem SQL: Abordagem Prática Para Inicianteso",
+                    imagem: "https://m.media-amazon.com/images/I/711siL1zU1L._SL1297_.jpg",
+                    editora: "Novatec",
+                    ano: 2016,
+                    ondeVender: "https://www.amazon.com.br/Introdu%C3%A7%C3%A3o-Linguagem-SQL-Abordagem-Iniciantes/dp/8575225014/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3UJJC6CH577CV&dib=eyJ2IjoiMSJ9.P_3MQOzj3jRXCCxyJ0F9zMu09JaiKNeim1nGF3cy6WYt1Bk0BXSplTj7HuKZQbFW5pAGz8vTX-62L3HNV1gZFoTrIuKqzE3o5aybtH7yJnuKLK9HGr_hgIOKh3_RHMApyCZZzTIKu00M3Rwt9XxtgTaYAG4E989V0lhQ1dvyqbeAjsrnKLiK1VE5FkN0uJ78N_QZKFw_QGVAg9gIVWzPTZbI85dQtKJk6dxzn_ost3E.2baWfQOrIbflTLP6cIztCPZp-ytm-YzSXij_ojlPOV0&dib_tag=se&keywords=introdu%C3%A7%C3%A3o+a+sql&qid=1728260959&s=books&sprefix=introdu%C3%A7%C3%A3o+a+sq%2Cstripbooks%2C301&sr=1-1"
+                },
+                {
+                    titulo: "UML: Uma Abordagem Prática",
+                    imagem: "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/_ee40cc43f1ed3010fbe24d20881df68591466902.jpg",
+                    editora: "Novatec",
+                    ano: 2006,
+                    ondeVender: "https://www.estantevirtual.com.br/livro/uml-uma-abordagem-pratica-19M-1499-000"
+                }
+            ],
+            jogos: [
+                {
+                    titulo: "Unity: Design e Desenvolvimento de Jogos",
+                    imagem: "https://m.media-amazon.com/images/I/81UIeOIxS2L._SL1500_.jpg",
+                    editora: "Alta Books",
+                    ano: 2019,
+                    ondeVender: "https://www.amazon.com.br/UNITY-Desenvolvimento-William-Pereira-Alves/dp/855080732X"
+                },
+                {
+                    titulo: "Console Wars: Sega, Nintendo, and the Battle That Defined a Generation",
+                    imagem: "https://m.media-amazon.com/images/I/71tCIRPfQPL._SL1500_.jpg",
+                    editora: "Dey Street Books",
+                    ano: 2015,
+                    ondeVender: "https://www.amazon.com.br/Console-Wars-Nintendo-Defined-Generation/dp/0062276700/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1Z0M5KDZ8F97M&dib=eyJ2IjoiMSJ9.9H83K26cvfT76CiIknQrrN2cATgKpQw4nUEfZU-czWCQ8mfuGuEmyxxb0IgVSVERcEc4qtM_wmkZWvHQd5wsCvtx1DrWRbfl8wdhsSNQC-FDb3fGBujj3oyjaw4GgeUnN_inGYwt4vGhUUNBPMUcQ9WqQ4Iyiw8bNSqXlDjeFr4Ll3viPwD3JZV8jEX-f693Xtud3PXs7JfeUXOKfJwA7g-7cNbPcAleRj12YoYpLCo.jjgQz2oXuEDUGtQWHDMzvhOjwXOPvFto8eIT25DB4wE&dib_tag=se&keywords=console+wars&qid=1728261842&s=books&sprefix=console+war%2Cstripbooks%2C271&sr=1-1&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d"
+                }
+            ]
+        };
+
+        const categoriaSelect = document.getElementById('categoria');
+        const livrosContainer = document.getElementById('livros-container');
+
+        categoriaSelect.addEventListener('change', function () {
+            const categoriaSelecionada = this.value;
+            livrosContainer.innerHTML = ''; // Limpar os livros exibidos
+
+            if (livros[categoriaSelecionada]) {
+                livros[categoriaSelecionada].forEach(livro => {
+                    const livroDiv = document.createElement('div');
+                    livroDiv.className = 'livro';
+                    livroDiv.innerHTML = `
+                        <img src="${livro.imagem}" alt="${livro.titulo}">
+                        <h2>${livro.titulo}</h2>
+                        <p class="detalhes">Editora: ${livro.editora}</p>
+                        <p class="detalhes">Ano: ${livro.ano}</p>
+                        <a href="${livro.ondeVender}" target="_blank">Comprar</a>
+                    `;
+                    livrosContainer.appendChild(livroDiv);
+                });
+            }
+        });
+   
